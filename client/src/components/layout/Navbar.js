2a0f2ab3e3145cc047f17b68bd6cc831474c5ad6
MariusHendriks <Mark.hendriks9999@gmail.com>
2019-02-05 23:37:02 +0100

    removed even more shit

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,17 +3,15 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
-import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
-    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
   render() {
-    const { isAuthenticated, user } = this.props.auth;
+    const { isAuthenticated } = this.props.auth;
 
     const authLinks = (
       <ul className="navbar-nav ml-auto">
@@ -22,11 +20,7 @@ class Navbar extends Component {
             Post Feed
           </Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/dashboard">
-            Dashboard
-          </Link>
-        </li>
+
         <li className="nav-item">
           <a
             href=""
@@ -39,20 +33,7 @@ class Navbar extends Component {
       </ul>
     );
 
-    const guestLinks = (
-      <ul className="navbar-nav ml-auto">
-        {/* <li className="nav-item">
-          <Link className="nav-link" to="/register">
-            Sign Up
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">
-            Login
-          </Link>
-        </li> */}
-      </ul>
-    );
+    const guestLinks = <ul className="navbar-nav ml-auto" />;
 
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
@@ -70,14 +51,7 @@ class Navbar extends Component {
           </button>
 
           <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/profiles">
-                  {" "}
-                  Developers
-                </Link>
-              </li> */}
-            </ul>
+            <ul className="navbar-nav mr-auto" />
             {isAuthenticated ? authLinks : guestLinks}
           </div>
         </div>
@@ -97,5 +71,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser, clearCurrentProfile }
+  { logoutUser }
 )(Navbar);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,8 +5,6 @@ const passport = require("passport");
 
 // Post model
 const Post = require("../../models/Post");
-// Profile model
-const Profile = require("../../models/Profile");
 
 // Validation
 const validatePostInput = require("../../validation/post");
@@ -76,21 +74,17 @@ router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Profile.findOne({ user: req.user.id }).then(profile => {
-      Post.findById(req.params.id)
-        .then(post => {
-          // Check for post owner
-          if (post.user.toString() !== req.user.id) {
-            return res
-              .status(401)
-              .json({ notauthorized: "User not authorized" });
-          }
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check for post owner
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notauthorized: "User not authorized" });
+        }
 
-          // Delete
-          post.remove().then(() => res.json({ success: true }));
-        })
-        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
-    });
+        // Delete
+        post.remove().then(() => res.json({ success: true }));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
   }
 );
 
